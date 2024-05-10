23d041986c8725bf309df1e2834bdc6864e4216b
gerardorosetti <gasayago@gmail.com>
2024-05-09 22:28:09 -0400

    first commit

---
+++ datatypes.hpp
@@ -0,0 +1,69 @@
+#include <cstdint>
+#include <set>
+
+class Matrix {
+
+private:
+    uint64_t rows;
+    uint64_t colums;
+
+};
+
+class Point {
+
+private:
+    double x;
+    double y;
+
+};
+
+class SetOfPoints {
+
+private:
+    std::set<Point> points_set;
+
+};
+
+class Equation {
+
+};
+
+class Function {
+
+};
+
+class SystemOfEquations {
+
+};
+
+class FirstOrderDifferentialEquation {
+
+};
+
+class HigherOrderDifferentialEquation {
+
+};
+
+class IntegerValue {
+
+};
+
+class RealValue {
+
+};
+
+class Interval {
+
+};
+
+class Integral {
+
+};
+
+class Constants {
+
+};
+
+class Variable {
+
+};
